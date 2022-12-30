17fdf2d3700fdc0e60ea905d485cef3189d7cd25
Roman-code-web <selenerg.275@gmail.com>
2022-12-30 14:47:46 -0500

    formulario terminado con alerta

--- src/app/componentes/body/formulario/formulario.component.ts
+++ src/app/componentes/body/formulario/formulario.component.ts
@@ -7,18 +7,35 @@ import Swal from 'sweetalert2';
   styleUrls: ['./formulario.component.css']
 })
 export class FormularioComponent {
+  verificacion!:boolean;
+  disabled=false;
+  colorclass!:string;
   dataArray:any=[]=[];
   @Output() onEnviarDatos =  new EventEmitter();
-
+  
   agregarDatos(nombre:string, apellido:string, edad:string){
     //this.dataArray.push(nom:nombre,String(apellido),String(edad));
     this.dataArray.push({
       "nom": nombre, "ape":apellido,"edad":edad
     })
     this.onEnviarDatos.emit(this.dataArray);
-    Swal.fire({
-      icon: 'success',
-      title: 'Datos Registrados',
-    })
+      Swal.fire({
+        icon: 'success',
+        title: 'Datos Registrados',
+      })
   }
+  
+  /*
+  validacion(campo:HTMLInputElement){
+    if(campo.tagName=="nombre" || campo==null || campo==undefined){
+      this.colorclass="is-invalid";
+      this.verificacion=false;
+      this.disabled=true;
+    }else{
+      this.colorclass="is-valid";
+      this.verificacion=true;
+      this.disabled=false;
+  }
+  */
+
 }
